c5110d47a33a53d2a0f1508f26e6a354fa3b41ac
Daniel Brooker <dan@nocturnalcode.com>
2017-06-26 13:12:28 +1200

    Change fastboot check

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports = {
   included: function(app) {
     this._super.included(app);
 
-    if (process.env.EMBER_CLI_FASTBOOT !== 'true') {
+    if (typeof FastBoot === 'undefined') {
       app.import(app.bowerDirectory + '/bootstrap-datepicker/dist/js/bootstrap-datepicker.js');
       app.import(app.bowerDirectory + '/bootstrap-datepicker/dist/css/bootstrap-datepicker.css');
     }
